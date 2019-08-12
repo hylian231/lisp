7f665763408fa6d377c6228f718c6bbeda5c1f06
hylian231 <hylia231@gmail.com>
2019-08-12 03:59:20 +0000

    add

--- occurrences.lisp
+++ occurrences.lisp
@@ -3,7 +3,7 @@
 
 (defun check (lst records)
   (if (not (consp lst))
-    records
+    (sort-decending records)
     (let ((s (pop lst)))
       (if (null(assoc s records))
 	(setf records (cons (list s 1) records))
@@ -24,6 +24,36 @@
     (setf lst (append lst (list x)))
     lst))
 
+(defun exchange(lst)
+ (if (< (second (car lst))
+	(second (second lst)))
+   (let ((upper (pop lst))
+	 (lower (pop lst)))
+     (push upper lst)
+     (push lower lst)
+     lst)
+   lst))
+
+(defun send-lower (lst)
+  (if (null (cdr lst))
+    lst
+    (progn
+      (setf lst (exchange lst))
+      (setf lst (cons (car lst) (send-lower (cdr lst))))
+      lst)))
+
+(defun sort-decending (lst)
+  (if (null (cdr lst))
+    lst
+    (let ((len (length lst)))
+      (setf lst (send-lower lst))
+      (setf lst (append (sort-decending (subseq lst 0 (- len 1)))
+			(list (nth (- len 1) lst))))
+      lst)))
+
+(format t "~A~%" (occurrences '(a a b b a b c c d d e e e))) 
 ;(format t "~A~%" (rotate '((a 1) (b 2) (c 3) (d 4)))) 
 ;(format t "~A~%" (counts 'b '((a 1) (b 2) (c 3) (d 4)))) 
-(format t "~A~%" (occurrences '(a a b b a b c c d d e e e))) 
+;(format t "~A~%" (exchange '((a 1) (b 2) (c 3) (d 4)))) 
+;(format t "~A~%" (send-lower '((a 1) (b 2) (c 3) (d 4)))) 
+;(format t "~A~%" (sort-decending '((a 3) (b 2) (c 3) (d 4)))) 
