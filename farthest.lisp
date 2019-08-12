5d2c9df3556d12d1d44c9bac3e249071057c75df
hylian231 <hylia231@gmail.com>
2019-08-12 10:37:45 +0000

    worked

--- farthest.lisp
+++ farthest.lisp
@@ -1,3 +1,7 @@
+(defun farthest (start goal net)
+  (let ((reached (advent (list start) goal net nil nil)))
+    (reverse (get-longest nil reached))))
+
 (defun get-newpath (path net)
   (let ((new (assoc (car path) net)))
     (if (null new)
@@ -13,55 +17,51 @@
       (setf new (push obj new))))
   new))
 
-;(defun advent (path goal net queue reached)
-  ;(if (equal goal (car path))
-    ;(progn
-      ;(push path reached)
-      ;(if (null (car queue))
-	;reached
-	;(advent (pop queue) goal net queue reached)))
-    ;(let ((new (get-newpath path net)))
-      ;(setf new (delete-duplicate new))
-      ;(if (not(null (car new)))
-	;(append queue new))
-      ;(advent (pop queue) goal net queue reached))))
-
-;(defun advent (path goal net queue reached)
-  ;(if (equal goal (car path))
-    ;(isgoal path goal net queue reached)
-    ;(let ((new (get-newpath path net)))
-      ;(setf new (delete-duplicate new))
-      ;(if (not(null (car new)))
-	;(append queue new))
-      ;(advent (pop queue) goal net queue reached))))
-
-;(defun isgoal (path goal net queue reached)
-  ;(push path reached)
-  ;(if (null (car queue))
-    ;reached
-    ;(advent (pop queue) goal net queue reached)))
-
-(defun isgoal (path goal net queue reached)
-  (push path reached)
-  (restpath goal net queue reached))
-
-(defun isnotgoal (path goal net queue reached)
-  (let ((new (get-newpath path net)))
-    (setf new (delete-duplicate new))
-    (if (not(null (car new)))
-      (append queue new))
-    (restpath goal net queue reached)))
-
-(defun restpath (goal net queue reached)
-  (if (null (car queue))
-    reached
-    (advent (pop queue) goal net queue reached)))
-
 (defun advent (path goal net queue reached)
   (if (equal goal (car path))
     (isgoal path goal net queue reached)
     (isnotgoal path goal net queue reached)))
 
-(format t "~A~%" (advent '(c b a) 'e '((a b c) (c d f)) '((e g h)) nil))
+(defun isgoal (path goal net queue reached)
+  (push path reached)
+  (nextpath goal net queue reached))
+  
+(defun isnotgoal (path goal net queue reached)
+  (let ((new (get-newpath path net)))
+    (setf new (delete-duplicate new))
+    (if (not(null (car new)))
+      (setf queue (append queue new)))
+    (nextpath goal net queue reached)))
+
+(defun nextpath (goal net queue reached)
+  (if (null (car queue))
+    reached
+    (advent (pop queue) goal net queue reached)))
+
+(defun get-longest (longest lst)
+  (if (null (cdr lst))
+    longest
+    (if (< (length longest) (length (car lst)))
+      (get-longest (car lst) (cdr lst))
+      (get-longest longest (cdr lst)))))
+
+;test code
+(format t "~A~%" (farthest 'a 'h '((a b c)
+				   (b a c d)
+				   (c a d e f)
+				   (d b c a h i)
+				   (h b a c d e i)
+				   (i f h)
+				   (f g o p)
+				   (p s d f a q l)
+				   (l g h j z n)
+				   (n o p s f q)
+				   (o g h))))
+
+;(format t "~A~%" (get-longest nil '((a b c) (a b) (a b c d e) (a) (a b c d))))
+;(isnotgoal '(c b a) 'e '((a b c) (c d e a b f g)) '((e g h)) nil)
+;(isgoal '(c b a) 'e '((a b c) (c d f)) '((e g h)) nil)
+;(format t "~A~%" (isgoal '(c b a) 'e '((a b c) (c d f)) '((e g h)) nil))
+;(format t "~A~%" (advent '(c b a) 'e '((a b c) (c d f)) '((e g h)) nil))
 ;(format t "~A~%" (delete-duplicate '((c c b a) (c b a) (c b c d a) (c d a) (b d b c) (b c d))))
 ;(format t "~A~%" (get-newpath '(c b a) '((a b c) (c d f))))
